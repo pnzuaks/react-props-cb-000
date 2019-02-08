c566a1010f5d997d53f3a9b587fa8a8a0ce24784
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-08 21:41:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
 
-ReactDOM.render(
-  <MovieCard
-    title="Jurassic World"
-    poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
-    genres={jurassicWorldGenres}
-  />,
-  document.getElementById('root')
-);
