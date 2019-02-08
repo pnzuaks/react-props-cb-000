edaca9bb3feca87ee46b0dc256ae336cde2d967f
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-08 21:43:18 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// Code goes here
+class MovieCard extends React.Component {
+  render() {
+    <MovieCard
+      title="Jurassic World"
+      poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
+      genres={jurassicWorldGenres}
+    />,
+    document.getElementById('root')
+  );
+  }
+}
+
+MovieCard.defaultProps = {
+  poster: 'http://i.imgur.com/bJw8ndW.png'
+};
+
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    genres={jurassicWorldGenres}
+  />,
+  document.getElementById('root')
+);
